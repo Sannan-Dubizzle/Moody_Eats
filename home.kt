734e18d344815686e695b4b2b9e989feb9b56591
Sannan-yousuf <84342579+Sannan-yousuf@users.noreply.github.com>
2022-05-20 23:47:23 +0500

    Add files via upload

---
+++ home.kt
@@ -0,0 +1,13 @@
+package com.example.moodyeats
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class home:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home)
+
+
+    }
+}
